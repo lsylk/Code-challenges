d7653b8a44a2aa449facdf068d1b288ca1243c9c
Leslye Lu <Leslye@Leslyes-MacBook-Pro.local>
2017-04-04 17:31:42 +0200

    Add if statement to check ifwordList is an array or multiple string arguments.

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -1,5 +1,30 @@
-var Anagram = function(){};
-
-Anagram.prototype.matches = function(words){
-    
+var Anagram = function(word){
+    this.word = word;
 };
+
+Anagram.prototype.matches = function(wordList){
+
+    var words = wordList;
+    if (!Array.isArray(words)) {
+        words = arguments;
+    }
+
+    var sortedWord = this.word.toLowerCase().split('').sort().join('');
+
+    var anagramWords = [];
+
+    for (var i = 0; i < words.length; i++){
+
+        if (sortedWord === words[i].toLowerCase().split('').sort().join('')) {
+
+            if (this.word.toLowerCase() !== words[i].toLowerCase()){
+                anagramWords.push(words[i]);
+            }
+            
+        }
+    }
+
+    return anagramWords;
+};
+
+module.exports = Anagram;
--- beer-song/beer-song.js
+++ beer-song/beer-song.js
@@ -1,15 +1,56 @@
-var BeerSong = function(numBottles){
-    this.numBottles = numBottles;
-};
+var BeerSong = function(){};
 
-BeerSong.prototype.verse = function() {
+BeerSong.prototype.verse = function(numBottles) {
 
-    if (this.numBottles > 0) {
-        return this.numBottles + ' bottles of beer on the wall, ' + this.numBottles + ' bottles of beer.' + ' Take one down and pass it around, ' + (this.numBottles - 1) + ' bottles of beer on the wall.';
+    var currentBottles = '';
+    var leftBottles = '';
+    var numTaken = '';
+
+    if (numBottles > 1) {
+
+        currentBottles = numBottles + ' bottles';
+        leftBottles = numBottles - 1 + ' bottles';
+        numTaken = 'one';
     }
 
+    if (numBottles === 2) {
+
+        currentBottles = numBottles + ' bottles';
+        leftBottles = numBottles - 1 + ' bottle';
+        numTaken = 'one';
+    }
+
+    if (numBottles === 1) {
+
+        currentBottles = numBottles + ' bottle';
+        leftBottles = 'no more bottles';
+        numTaken = 'it';
+    }
+
+    if (numBottles === 0) {
+
+        return `No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n`;
+    }
+
+    return `${currentBottles} of beer on the wall, ${currentBottles} of beer.\nTake ${numTaken} down and pass it around, ${leftBottles} of beer on the wall.\n`
+
 };
 
-// Beerson.prototype.sing(){};
+BeerSong.prototype.sing = function(start, stop){
+
+    //Set default value for stop as 0.
+
+    stop = stop || 0;
+
+    var totalVerses = '';
+
+    for (var i = start; i >= stop; i--) {
+
+        totalVerses += this.verse(i) + (i != stop ? '\n': '');
+    }
+
+    return totalVerses;
+
+};
 
 module.exports = BeerSong;
